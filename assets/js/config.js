fba2ac1de920e9518fe12c5211997ef423137e24
Oluwatobi Ekundayo <tobiekundayo07@gmail.com>
2023-08-19 13:52:13 +0100

    Initial Commit

---
+++ assets/js/config.js
@@ -0,0 +1,27 @@
+/**
+ * Config
+ * -------------------------------------------------------------------------------------
+ * ! IMPORTANT: Make sure you clear the browser local storage In order to see the config changes in the template.
+ * ! To clear local storage: (https://www.leadshook.com/help/how-to-clear-local-storage-in-google-chrome-browser/).
+ */
+
+'use strict';
+
+// JS global variables
+let config = {
+  colors: {
+    primary: '#C4E7D4',
+    secondary: '#C4DACF',
+    success: '#B9C0DA',
+    info: '#C4DACF',
+    warning: '#ffab00',
+    danger: '#ff3e1d',
+    dark: '#233446',
+    black: '#000',
+    white: '#fff',
+    body: '#f4f5fb',
+    headingColor: '#566a7f',
+    axisColor: '#a1acb8',
+    borderColor: '#eceef1'
+  }
+};
